c80f957e47c895fd83f663da982aaa12bf1329c3
f2hard3 <f2hard3@gmail.com>
2019-04-29 17:21:30 +0900

    feat add jetsetter

--- app/main.js
+++ app/main.js
@@ -1,11 +1,22 @@
 import { app, BrowserWindow } from 'electron';
+import { enableLiveReload } from 'electron-compile';
+
+enableLiveReload({ strategy: 'react-hmr' });
 
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ width: 300, height: 600, minWidth: 300, show: false });
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
+    mainWindow = new BrowserWindow({
+        width: 300,
+        height: 600,
+        minWidth: 300,
+        show: false,
+        webPreferences: {
+          nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile(`${__dirname}/index.html`);
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
 });
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,50 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-render(<h1>Hello World!</h1>, document.getElementById('application'));
+// const Application = () => {
+//     return (
+//         <div>
+//             <h1>Hello World!</h1>
+//             <button className="full-width">
+//                 This button does not do anything.
+//             </button>
+//         </div>
+//     )
+// }
+
+const renderApplication = () => {
+    const { default: Application } = require('./components/Application');
+    render(
+        <AppContainer>
+            <Application />
+        </AppContainer>,
+        document.getElementById('application')
+    );
+};
+
+// const renderApplication = () => {
+//     import('./components/Application').then(({ default: Application }) => {
+//         render(
+//             <AppContainer>
+//                 <Application />
+//             </AppContainer>,
+//             document.getElementById('application')
+//         );
+//     });
+// };
+
+// const renderApplication = async () => {
+//     const { default: Application } = await import('./components/Application');
+//         render(
+//             <AppContainer>
+//                 <Application />
+//             </AppContainer>,
+//             document.getElementById('application')
+//         );
+
+// };
+
+renderApplication();
+
+if (module.hot) module.hot.accept(renderApplication);
